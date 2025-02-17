686155ef2029adfd1f265f90effa00a4861df32e
Timofey-Bidenko <timofeytimtimofey@gmail.com>
2025-02-17 07:07:28 +0100

    HW6

--- EntryPoints/index.js
+++ EntryPoints/index.js
@@ -1,7 +1,35 @@
-import Logger from "../CustomModules/logger/index.js"
+import http from "node:http"
+import wait from "../CustomModules/luauWait/index.js"
+import random from "../CustomModules/mathRandom/index.js"
 
-const logger = new Logger()
+const APP_PORT = 3000
+const server = http.createServer()
 
-logger.getAllMethods().forEach((methodName, i) => {
-    logger[methodName]("Hello World!")
+/* 
+TODO
+
+Реалізувати свій сервер на node http, який вибірково (10% запитів) повертає 500 помилку, а всі інші запити відпрацьовують з затримкою від 1 до 3 секунд. 
+
+Докладніше: сервер має повертати response з затримкою, від 1 до 3 секунд, цей час має бути рандомним, а також приблизно 10% запитів "падають" з помилками 500
+*/
+
+let requestsTotal = 0
+
+server.on("request", async (req, res) => {
+    requestsTotal++
+
+    const isError = Math.random() >= 0.9
+    res.writeHead(isError ? 500 : 200, {
+        "content-type": "text/plain"
+    })
+    await wait(random(1000, 3000))
+
+    if (isError) return res.end(`Internal Server Error (${requestsTotal})`);
+    res.end(`Hello World! ${requestsTotal}`)
+})
+
+
+server.listen(APP_PORT, () => {
+    console.log(`HTTP Server is listening on port ${APP_PORT}
+Go to: http://localhost:${APP_PORT}`)
 })
