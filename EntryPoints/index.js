6dfeaf6b10518ec7fd775d5d4c08b7c0735f7123
Timofey-Bidenko <timofeytimtimofey@gmail.com>
2025-02-17 19:34:43 +0100

    save

--- EntryPoints/index.js
+++ EntryPoints/index.js
@@ -1,35 +1,45 @@
-import http from "node:http"
-import wait from "../CustomModules/luauWait/index.js"
-import random from "../CustomModules/mathRandom/index.js"
+import path from "node:path";
+import {fileURLToPath} from "node:url";
+import express, {json} from "express";
+
+const __fileName = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__fileName)
+global.__fileName = __fileName
+global.__dirname = __dirname
 
 const APP_PORT = 3000
-const server = http.createServer()
+
+const app = express();
+
+// app.all("/", (req, res) => {
+//     res.send("Hello, World!");
+
+//     // res.redirect("")
+// });
+
+// app.get("/", (req, res) => {
+//    res.send("Hello, World!");
+// });
+
+// Patterns: example: ab*cd our route has to start with ab and end with cd, anything in between is still valid, because of * sign
+app.get("/ab*cd", (req, res) => {
+    res.send(req.url);
+});
+
+app.listen(APP_PORT, () => {
+    console.log(`Server listening on port ${APP_PORT}
+Link: http://localhost:${APP_PORT}`);
+});
 
 /* 
-TODO
+Використовувати Express.js для створення веб-сервера.
 
-Реалізувати свій сервер на node http, який вибірково (10% запитів) повертає 500 помилку, а всі інші запити відпрацьовують з затримкою від 1 до 3 секунд. 
+Реалізація RESTful API для роботи з даними (GET, POST, PUT, DELETE запити).
 
-Докладніше: сервер має повертати response з затримкою, від 1 до 3 секунд, цей час має бути рандомним, а також приблизно 10% запитів "падають" з помилками 500
+Створити додаток TO-DO, який дозволяю:
+
+додати нові таски,  (POST /items)
+переглянути існуючі,  (GET /items) 
+міняти статус (new/done)  (PUT /items/:itemId)
+видаляти таски (DELETE /items/:itemId)
 */
-
-let requestsTotal = 0
-
-server.on("request", async (req, res) => {
-    requestsTotal++
-
-    const isError = Math.random() >= 0.9
-    res.writeHead(isError ? 500 : 200, {
-        "content-type": "text/plain"
-    })
-    await wait(random(1000, 3000))
-
-    if (isError) return res.end(`Internal Server Error (${requestsTotal})`);
-    res.end(`Hello World! ${requestsTotal}`)
-})
-
-
-server.listen(APP_PORT, () => {
-    console.log(`HTTP Server is listening on port ${APP_PORT}
-Go to: http://localhost:${APP_PORT}`)
-})
