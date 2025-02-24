49a2a5af18f6920004749b95be95710b755c1778
Timofey-Bidenko <timofeytimtimofey@gmail.com>
2025-02-24 12:19:08 +0100

    Lesson 11, custom project

--- EntryPoints/index.js
+++ EntryPoints/index.js
@@ -1,54 +1,59 @@
-import express, { json } from "express";
+import express, { json, Router } from "express";
 const app = express();
 app.use(json());
+app.set("view engine", "pug");
+app.set("views", "./views/"); // Ensure ./index.pug is in the root
 
+const App_Port = 3000
+const linkRoot = `http://localhost:${App_Port}/`
 
-
-const DS = {}
+const DS = {};
 
 app.post("/:mainKey", (req, res) => {
-    if (!req.body || !req.body.text) return res.status(400).json({error: `"text": "[Your text here]" in a json is required`});
-
-    const mainKey = req.params.mainKey
-
-    if (!DS[mainKey]) DS[mainKey] = {
-        localId: 0,
-        items: []
+    let textRequired = true
+    const mainKey = req.params.mainKey;
+    
+    if (!DS[mainKey]) {
+        DS[mainKey] = { localId: 0, items: [] }
+        textRequired = false
     };
-    const { localId, items } = DS[mainKey]
-    const newId = localId + 1
 
+    if (!textRequired) res.json(DS);
+
+
+
+    if (!req.body || !req.body.text) return res.status(400).json({ error: `"text": "[Your text here]" in a json is required` });
+    
+    const newId = DS[mainKey].localId + 1;
     const item = { id: newId, text: req.body.text, status: "new" };
-    items.push(item);
-    DS[mainKey].localId = newId
+    DS[mainKey].items.push(item);
+    DS[mainKey].localId = newId;
+
     res.json(item);
 });
 
 app.get("/:mainKey", (req, res) => {
-    const mainKey = req.params.mainKey
-
+    const mainKey = req.params.mainKey;
     if (!DS[mainKey]) return res.sendStatus(404);
-    res.json(DS[mainKey].items)
+    
+    res.render("index", { linkRoot: linkRoot, tasks: DS[mainKey].items, currentUrl: `/${mainKey}` });
 });
+
 app.get("/:mainKey/:itemId", (req, res) => {
-    const { mainKey, itemId } = req.params
-
+    const { mainKey, itemId } = req.params;
     if (!DS[mainKey]) return res.sendStatus(404);
-    const items = DS[mainKey].items
-
-    const item = items.find(i => i.id === parseInt(itemId));
+    
+    const item = DS[mainKey].items.find(i => i.id === parseInt(itemId));
     if (!item) return res.sendStatus(404);
 
     res.json(item);
 });
 
 app.put("/:mainKey/:itemId", (req, res) => {
-    const { mainKey, itemId } = req.params
-
+    const { mainKey, itemId } = req.params;
     if (!DS[mainKey]) return res.sendStatus(404);
-    const items = DS[mainKey].items
-
-    const item = items.find(i => i.id === parseInt(itemId));
+    
+    const item = DS[mainKey].items.find(i => i.id === parseInt(itemId));
     if (!item) return res.sendStatus(404);
 
     item.status = item.status === "new" ? "done" : "new";
@@ -56,20 +61,18 @@ app.put("/:mainKey/:itemId", (req, res) => {
 });
 
 app.delete("/:mainKey/:itemId", (req, res) => {
-    const { mainKey, itemId } = req.params
-
+    const { mainKey, itemId } = req.params;
     if (!DS[mainKey]) return res.sendStatus(404);
-    let items = DS[mainKey].items
-
-    const lastLength = items.length;
-    DS[mainKey].items = items.filter(i => i.id !== parseInt(itemId));
+    
+    const lastLength = DS[mainKey].items.length;
+    DS[mainKey].items = DS[mainKey].items.filter(i => i.id !== parseInt(itemId));
     if (DS[mainKey].items.length === lastLength) return res.sendStatus(404);
 
     res.sendStatus(200);
 });
 
 app.all("*", (req, res) => {
-    res.json(DS);
-})
+    res.render("index", { linkRoot: linkRoot, mainKeys: Object.keys(DS), tasks: [], currentUrl: `/` });
+});
 
-app.listen(3000, () => console.log("Server running on port 3000 >>> http://localhost:3000/"));
+app.listen(App_Port, () => console.log(`Server running on port App_Port >>> ${linkRoot}`));
